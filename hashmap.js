aba9474c599bd75b222e29e31ad23ba3667da299
safwansadi <safwaniftekher@gmail.com>
2022-09-11 18:32:10 +0600

    Most frequent element

---
+++ hashmap.js
@@ -0,0 +1,7 @@
+let hashmap = new Map();
+
+hashmap.set("number", 700);
+hashmap.set("array", [1, 2, 3]);
+
+console.log(typeof hashmap);
+console.log(hashmap);
---
+++ mostfrequentelement.js
@@ -0,0 +1,27 @@
+function mostFrequent(arr, n) {
+  // Insert all elements in hash.
+  var hash = new Map();
+  for (var i = 0; i < n; i++) {
+    if (hash.has(arr[i])) hash.set(arr[i], hash.get(arr[i]) + 1);
+    else hash.set(arr[i], 1);
+  }
+
+  // find the max frequency
+  var max_count = 0,
+    res = -1;
+  hash.forEach((value, key) => {
+    if (max_count < value) {
+      console.log(value);
+      res = key;
+      max_count = value;
+    }
+  });
+
+  return res;
+}
+
+// driver program
+var arr = [40, 50, 30, 40, 50, 30, 30];
+var n = arr.length;
+
+console.log(mostFrequent(arr, n));
